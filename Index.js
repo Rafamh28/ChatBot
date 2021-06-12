9787c1f2c9102f90cbd5e4bbeba2cdd44aa5cd4f
Lawrence Rafael <rafaelfebrero2802@gmail.com>
2021-06-12 19:26:50 -0400

    Comandos basicos del bot!
    Comandos basicos!

--- Index.js
+++ Index.js
@@ -6,4 +6,16 @@
     //Enlace al bot mediante el token!
     const bot = new Telegraf('1659752930:AAGZ01r7p1PaKFjZyJ4rjIJ9p3THFTAVUtY')
 
+    bot.start(ctx =>{
+        ctx.reply("Hola, mi nombre es Gary. En que te puedo ayudar " + ctx.chat.username + "?")
+    })
 
+    bot.on('sticker', ctx => {
+        ctx.reply("Te gustan los stickers?")
+    })
+
+    bot.on('text', ctx => {
+        ctx.reply("No puedo responderte de manera adecuada en este momento.")
+    })
+    //Lanzamiento del bot
+    bot.launch()
