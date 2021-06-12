5b29630f4044483aad40e672febec3492ba40360
Rafael <rafaelfebrero2802@gmail.com>
2021-06-12 16:58:59 -0400

    Index.js // Cuerpo del bot
    Archivo que contiene el cuerpo del bot

---
+++ Index.js
@@ -0,0 +1,9 @@
+//Archivo principal del Bot
+
+    //Variable de enlace de control del bot
+    const { Telegraf } = require('Telegraf')
+    
+    //Enlace al bot mediante el token!
+    const bot = new Telegraf('1659752930:AAGZ01r7p1PaKFjZyJ4rjIJ9p3THFTAVUtY')
+
+
